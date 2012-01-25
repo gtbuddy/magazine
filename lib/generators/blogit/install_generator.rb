b5f9a644bec321597000a2dc9265325d4c783d1c
Galen <galenmail@gmail.com>
2012-01-24 19:02:28 -0600

    Moving blog generator into generators file.

--- lib/generators/blogit/install_generator.rb
+++ lib/generators/blogit/install_generator.rb
@@ -11,5 +11,25 @@ module Blogit
       end
 
     end
+
+		class BlogGenerator < Rails::Generators::Base
+		  include Rails::Generators::Migration
+		  def self.source_root
+		    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+		  end
+
+		  def self.next_migration_number(dirname)
+		    if ActiveRecord::Base.timestamped_migrations
+		      Time.new.utc.strftime("%Y%m%d%H%M%S")
+		    else
+		      "%.3d" % (current_migration_number(dirname) + 1)
+		    end
+		  end
+
+		  def create_migration_file
+		    migration_template 'create_magazine_posts.rb', 'db/migrate/create_magazine_posts.rb'
+		    migration_template 'create_magazine_comments.rb', 'db/migrate/create_magazine_comments.rb'
+		  end
+		end
   end
 end
