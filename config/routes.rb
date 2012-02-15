8ce74440c4fd92d6ae44c8d0a6a5aa2cf351195f
Marco Gallardo <marco.gallardo@tangosource.com>
2012-02-15 13:48:57 -0600

    changed routes from /magazine/articles/ to /m/a/

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   
   # Keep these above the articles resources block
-  match "magazine/articles/toggle_feature/:id" => 'articles#toggle_feature', :as => :toggle_feature_articles
-  match "magazine/articles/approve/:id" => 'articles#approve', :as => :approve_articles
-  match "magazine/articles/toggle_publish/:id" => 'articles#toggle_publish', :as => :toggle_publish_articles
-  match "articles/review/" => 'articles#review', :as => :review_articles
-  match "articles/page/:page" => "articles#index"
-  match "articles/tagged/:tag" => 'articles#tagged', :as => :tagged_articles
+  match "m/a/toggle_feature/:id" => 'articles#toggle_feature', :as => :toggle_feature_articles
+  match "m/a/approve/:id" => 'articles#approve', :as => :approve_articles
+  match "m/a/toggle_publish/:id" => 'articles#toggle_publish', :as => :toggle_publish_articles
+  match "a/review/" => 'articles#review', :as => :review_articles
+  match "a/page/:page" => "articles#index"
+  match "a/tagged/:tag" => 'articles#tagged', :as => :tagged_articles
     
   resources :articles do
     resources :comments, :only => [:create, :destroy]
   end
 
-  root :to => "articles#index"
+  root :to => "a#index"
 end
