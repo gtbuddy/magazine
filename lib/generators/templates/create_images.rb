705ef4d0da4cc68ce15c4ebd3bc0b06351d3c177
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-01 12:54:43 -0600

    fixes on create images migration

--- lib/generators/templates/create_images.rb
+++ lib/generators/templates/create_images.rb
@@ -1,10 +1,10 @@
 class CreateImages < ActiveRecord::Migration
   def self.up
     create_table :images do |t|
+      t.integer :article_id, :null => false
       t.string :url, :null => false
       t.timestamps
     end
-    add_index :images, :article_id
     add_column :articles, :excerpt, :text
   end
 
