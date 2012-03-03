acebe9d8e423703119a8569d13bff45aef1ce916
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-02 19:22:00 -0600

    saving image

--- app/controllers/magazine/articles_controller.rb
+++ app/controllers/magazine/articles_controller.rb
@@ -64,6 +64,12 @@ module Magazine
 
     def update
       @article = Article.find(params[:id])
+      @images = params[:images].split('≠')
+      @images.each do |image|
+        @image = Image.new :article_id => @article.id
+        @image.file = image
+        @image.save!
+      end
       if @article.update_attributes(params[:magazine_article])
         redirect_to @article, :notice => 'Article was successfully updated.'
       else
