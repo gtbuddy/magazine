68ae846d34f3cff194eced3a81c9a0cd3a1a76ef
Marco Gallardo <marco.gallardo@tangosource.com>
2012-03-02 18:11:06 -0600

    fixed class name for migration... again

--- lib/generators/templates/fix_images_field.rb
+++ lib/generators/templates/fix_images_field.rb
@@ -1,9 +1,9 @@
 class FixImagesField < ActiveRecord::Migration
   def self.up
-    rename_column :images, :file, :string, :null => false
+    rename_column :images, :file, :string
   end
 
   def self.down
-    rename_column :images, :url, :string, :null => false
+    rename_column :images, :url, :string
 	end
 end
